625dffc1d2c273caff198e8d3ca3db7313738aad
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-03-17 08:29:57 +0100

    latest

--- src/views/VoiceView.vue
+++ src/views/VoiceView.vue
@@ -1,33 +1,27 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
+import SliderControl from "@/components/SliderControll.vue"
+import { ref } from "vue";
+const vol = ref(0);
+const rate = ref(1);
+const pitch = ref(1);
 </script>
 
 <template>
-  <h2 class="title text-center text-light my-2">Text to Speech Settings</h2>
-  <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12 mx-3 my-2">
-    <div class="input-group mb-3">
-      {{ vol }}
-<SliderControl v-model="vol" label="Volume"/>
-    </div>
-    <div class="input-group mb-3">
-      <label for="customRange3" class="form-label">
-        <h2>Rate</h2>
-      </label>
-      <input type="range" class="form-range" value="0.8" min="0.5" max="2" step="0.1" id="customRange3">
-    </div>
-    <div class="input-group mb-3">
-      <label for="customRange3" class="form-label">
-        <h2>Pitch</h2>
-      </label>
-      <input type="range" class="form-range" value="1" min="0" max="2" step="0.1" id="customRange3">
-    </div>
-    
+<div class="row">
+<h2 class="text-light text-center"> Settings </h2>
+  <div class="col-lg-12 mb-4">
+    <SliderControl v-model="vol" label="Volume" :caliber="vol"/>
   </div>
+  <div class="col-lg-6">
+    <SliderControl v-model="rate" label="Rate" :caliber="rate" :min="0" :max="1" :step="0.1" />
+  </div>
+  <div class="col-lg-6">
+    <SliderControl v-model="pitch" label="Pitch" :caliber="pitch" :min="0" :max="1" :step="0.1" />
+  </div>
+<div class="col-3 mt-4">
+<button class="btn btn-sm btn-warning d-">Save</button>
+</div>
+</div>
 </template>
 
 <style scoped></style>
