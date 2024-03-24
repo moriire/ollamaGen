96f6a70cb802eea7e002872ba4a7e8fed90f6895
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-03-25 00:17:26 +0100

    latest

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -90,7 +90,7 @@ export default {
 </script>
 
 <template>
-  <div class="row justify-content-center .align-items-center">
+  <!--div class="row justify-content-center .align-items-center">
     <div v-show="disable" class="col-lg-10 col-md-10 col-sm-10 col-xs-12 mx-3 my-2">
       <div class="row justify-content-end">
         <div class="col-8">
@@ -109,18 +109,19 @@ export default {
         </div>
       </div>
     </div>
-    </div>
-    <div class="row justify-content-center .align-items-center">
-    <div class="col-lg-10 col-md-10 mx-3 mb-2 bg-grey text-light border border-top border-bottom-0 p-3 fs-3 overflow-y" id="gensys" v-show="response">
-      
-      {{  response }}
+  </div-->
+  <div class="row justify-content-center .align-items-center" id="main">
+    <div class="col-lg-10 col-md-10 mx-3 mb-2 bg-grey text-light border border-top border-bottom-0 p-3 fs-3 overflow-y"
+      id="gensys" v-show="response">
+
+      {{ response }}
       <!--textarea disabled readonly class="form-control bg-dark text-light overflow-auto board" v-model="response"
         id="cop">
 
         </textarea-->
     </div>
-    </div>
-    <!--div class="col-lg-10 col-md-10 col-sm-10 col-xs-12 mx-3 my-2 justify-content-between">
+  </div>
+  <!--div class="col-lg-10 col-md-10 col-sm-10 col-xs-12 mx-3 my-2 justify-content-between">
       <div>
         <button class="btn btn-outline-warning" v-show="disable" @click="stopGen">
           <i class="bi bi-stop-fill"></i>
@@ -142,35 +143,39 @@ export default {
         </h2>
       </div>
     </div-->
-    <div class="row justify-content-center .align-items-center mt-2">
+  <div class="row justify-content-center .align-items-center mt-2">
     <div
-      class="col-lg-10 col-md-10 col-sm-10 col-xs-12 mx-3 position-absolute start-50 bottom-0 translate-middle-x">
+      class="col-lg-10 col-md-10 col-sm-10 col-xs-12 mx-3 position-sticky .start-50 .bottom-0 sticky-bottom .translate-middle-x">
       <div class="row justify-content-between">
         <div class="col-8">
-        <button class="btn btn-outline-warning" v-show="disable" @click="stopGen">
-          <i class="bi bi-stop-fill"></i>
-        </button>
-        <button class="btn btn-outline-warning" @click="tts()" v-show="response">
-          <i class="bi bi-mic-fill"></i>
-        </button>
+          <button class="btn btn-outline-warning" v-show="disable" @click="stopGen">
+            <i class="bi bi-stop-fill"></i>
+          </button>
+          <button class="btn btn-outline-warning" @click="tts()" v-show="response">
+            <i class="bi bi-mic-fill"></i>
+          </button>
 
-        <button data-bs-toggle-tooltip="Copy AI response" class="btn btn-outline-warning" v-show="response"
-          @click="copyOutput">
-          <i class="bi bi-clipboard"></i></button>
-        <button title="Copy response to clipboard" data-bs-toggle="tooltip" data-bs-placement="top"
-          class="btn btn-outline-warning" @click="copyOutput" v-show="response">Copy</button>
-        <button class="btn btn-outline-warning" @click="clearOutput" v-show="response">Clear</button>
+          <button data-bs-toggle-tooltip="Copy AI response" class="btn btn-outline-warning" v-show="response"
+            @click="copyOutput">
+            <i class="bi bi-clipboard"></i></button>
+          <button title="Copy response to clipboard" data-bs-toggle="tooltip" data-bs-placement="top"
+            class="btn btn-outline-warning" @click="copyOutput" v-show="response">Copy</button>
+          <button class="btn btn-outline-warning" @click="clearOutput" v-show="response">Clear</button>
+        </div>
+        <div class="col-4">
+          <h2 class="text-light text-end" v-show="response">
+            {{ total_duration.toFixed(2) }} T/s
+          </h2>
+        </div>
       </div>
-      <div class="col-4">
-        <h2 class="text-light text-end" v-show="response">
-          {{ total_duration.toFixed(2) }} T/s
-        </h2>
-      </div>
-      </div>
-      <div class="input-group my-3">
+      <div class="input-group my-2">
         <!--button class="input-group-text bg-warning text-dark" id="basic" @click="tts()">
           <i class="bi bi-mic-fill"></i>
       </button-->
+        <button class="input-group-text bg-warning text-dark" id="basic" v-show="disable">
+          <span class="spinner-border spinner-border-sm" aria-hidden="true">
+          </span>
+        </button>
         <input class="form-control" aria-describedby="basic" placeholder="I am your AI Doctor. How are you feeling?"
           v-model="prompt" @keyup.enter="getChat()">
         <button class="input-group-text bg-warning text-dark" id="basic" @click="getChat()">
@@ -184,11 +189,16 @@ export default {
 textarea {
   outline-color: yellow;
 }
-#gensys{
+
+#gensys {
   min-width: min-content;
   max-width: max-content;
   min-height: min-content;
   max-height: 70vh;
   overflow-y: scroll;
+}
+
+div#main {
+  min-height: 70vh;
 }
 </style>
