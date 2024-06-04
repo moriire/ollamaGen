bc4927960af3fc784f5d9686ed4fa64e34cd31a8
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-06-04 11:56:26 +0100

    latest

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -2,6 +2,7 @@
 import "alertifyjs/build/css/alertify.min.css";
 import { ref } from "vue";
 import SliderControl from "./components/SliderControll.vue";
+import SideBar from "./components/SideBar.vue";
 import ThemeSwitcher from './components/ThemeSwitcher.vue';
 import { useVoiceSettingsStore } from './stores/counter.js';
 import { useGenerateStore } from "./stores/generate.js";
@@ -13,7 +14,7 @@ import {
   RouterView
 } from 'vue-router'
 export default {
-  components: { RouterLink, RouterView, SliderControl, ThemeSwitcher},
+  components: { RouterLink, RouterView, SliderControl, ThemeSwitcher, SideBar},
 
   setup() {
     const gen = useGenerateStore();
@@ -47,106 +48,18 @@ export default {
       </button>
     </div>
     <div class="offcanvas-body">
-      <div class="nav flex-column nav-pills nav-warning me-3" role="tablist" aria-orientation="vertical">
-        <RouterLink class="nav-link" to="/">Home</RouterLink>
-        <hr class="text-white">
-        <RouterLink class="nav-link" to="/chat">Chat</RouterLink>
-        <hr class="text-white">
-        <RouterLink class="nav-link" to="/models">Models</RouterLink>
-        <hr class="text-white">
-
-        <div class="row justify-content-center">
-          <div class="col-12 my-2">
-            <SliderControl v-model="params.modelParams.temperature" label="Temperature" :min="0" :max="1" :step=".1"
-              @change="params.changeModelParams" />
-          </div>
-
-          <!--div class="col-lg-12 my-2">
-            <SliderControl v-model="params.modelParams.top_p" label="Top_p" :caliber="params.modelParams.top_p" :min="0"
-              :max="1.0" :step="0.05" @change="params.changeModelParams" />
-          </div-->
-
-          <div class="col-lg-12 my-2">
-            <SliderControl v-model="params.modelParams.top_k" label="Top_k" :min="0" :max="100" :step="5"
-              @change="params.changeModelParams" />
-          </div>
-
-          <div class="col-lg-12 my-2">
-            <SliderControl v-model="params.modelParams.seed" label="seed" :min="0" :max="100"
-              @change="params.changeModelParams" />
-          </div>
-        </div>
-      </div>
+      <SideBar />
     </div>
   </div>
   <div class="container-fluid">
     <div class="row">
       <div class="col-lg-3 d-none col-lg-block d-none d-lg-block  bg-dark vh-100 px-4 py-4 sidebar"
         style="overflow-y: scroll; height:min-content">
-        <div class="navbar-brand text-warning mt-3 text-decoration-none" to="/">
+        <div class="navbar-brand text-warning mt-3" to="/">
 
           <h2>ollamaGen <span class="float-end"><ThemeSwitcher/></span></h2>
         </div>
-        <div class="nav flex-column nav-pills nav-warning me-3 mt-4" role="tablist" aria-orientation="vertical">
-          <RouterLink class="nav-link" to="/">Home</RouterLink>
-          <hr class="text-white">
-          <RouterLink class="nav-link" to="/chat" type="button">Chat</RouterLink>
-          <hr class="text-white">
-          <RouterLink class="nav-link" to="/models" >Models</RouterLink>
-          <hr class="text-white">
-          <div class="col-lg-12 my-2">
-            <!--div class="d-flex justify-content-between">
-              <input type="radio" class="btn-check" v-model="gen.resMode" value="batch" id="option1" autocomplete="off">
-              <label class="btn btn-outline-warning" for="option1">Batch</label>
-
-              <input type="radio" class="btn-check" v-model="gen.resMode" value="stream" id="option2"
-                autocomplete="off">
-              <label class="btn btn-outline-warning" for="option2">Stream</label>
-            </div>
-            <hr class="text-white"-->
-            <div class="nav flex-column nav-pills nav-warning">
-
-              <a class="nav-link d-flex justify-content-between bg-warning" type="button" href="#" data-bs-toggle="collapse" data-bs-target="#modelParameter"
-                  aria-expanded="false" aria-controls="collapseExample">
-                {{ params.selectedModel }}
-                <span class="me-0" ><i class="bi bi-caret-down"></i></span>
-              </a>
-
-              <div class="collapse show" id="modelParameter">
-                <div class="row justify-content-center ms-3">
-                  <div class="col-12 my-2">
-                    <SliderControl v-model="params.modelParams.temperature" label="Temperature" :min="0" :max="1"
-                      :step=".1" @change="params.changeModelParams" :caliber="params.modelParams.temperature" />
-                  </div>
-
-                  <div class="col-lg-12 my-2">
-                    <SliderControl v-model="params.modelParams.top_p" label="Top_p" :min="0" :max="1.0" :step="0.05"
-                      @change="params.changeModelParams" :caliber="params.modelParams.top_p" />
-                  </div>
-
-                  <div class="col-lg-12 my-2">
-                    <SliderControl v-model="params.modelParams.top_k" label="Top_k" :min="0" :max="100" :step="5"
-                      @change="params.changeModelParams" :caliber="params.modelParams.top_k" />
-                  </div>
-                  <div class="col-lg-12 my-2">
-                    <SliderControl v-model="params.modelParams.n_ctx" label="num_ctx" :min="126" :max="8192" :step="126"
-                      @change="params.changeModelParams" :caliber="params.modelParams.n_ctx" />
-                  </div>
-
-                  <!--div class="col-lg-12 my-2">
-              <SliderControl v-model="params.modelParams.seed" label="seed" :min="0" :max="100"
-                @change="params.changeModelParams" />
-            </div-->
-                  <!--div class="col-lg-12 my-3 text-center">
-              <button class="btn btn-sm btn-outline-warning" @click="reset">reset</button>
-            </div-->
-                </div>
-              </div>
-            </div>
-          </div>
-          <hr class="text-white">
-
-        </div>
+       <SideBar />
       </div>
       <div class="col-lg-9 main">
         <nav class="m-3 navbar navbar-expand-lg navbar-expand-md navbar-light d-lg-none  ">
@@ -206,5 +119,11 @@ div.cont {
 /* Handle on hover */
 ::-webkit-scrollbar-thumb:hover {
   background: #555;
+}
+
+.form-switch .form-check-input:checked {
+  background-position: right center;
+  background-color: var(--bs-dark);
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='%23fff'/%3e%3c/svg%3e");
 }
 </style>
---
+++ src/renderer/components/SideBar.vue
@@ -0,0 +1,93 @@
+<script>
+import "alertifyjs/build/css/alertify.min.css";
+import { ref } from "vue";
+import SliderControl from "./SliderControll.vue";
+import { useVoiceSettingsStore } from '../stores/counter.js';
+import { useGenerateStore } from "../stores/generate.js";
+
+import {
+  RouterLink,
+} from 'vue-router'
+export default {
+  components: { RouterLink, SliderControl},
+
+  setup() {
+    const gen = useGenerateStore();
+    const showMessage = async () => {
+      await alertify.alert('ollamaGen', `Response mode changed to ${resMode.value}`)
+    }
+    const params = useVoiceSettingsStore();
+    return {
+      params,
+      SliderControl,
+      RouterLink,
+      showMessage,
+      gen
+    }
+  }
+
+}
+
+</script>
+<template>
+<div class="nav flex-column nav-pills nav-warning me-3 mt-4" role="tablist" aria-orientation="vertical">
+          <RouterLink class="nav-link" to="/">Home</RouterLink>
+          <hr class="text-white">
+          <RouterLink class="nav-link" to="/chat" type="button">Chat</RouterLink>
+          <hr class="text-white">
+          <RouterLink class="nav-link" to="/models" >Models</RouterLink>
+          <hr class="text-white">
+         
+          <div class="col-lg-12 my-2">
+            <div class="d-flex justify-content-between">
+              <input type="radio" class="btn-check" v-model="gen.resMode" value="batch" id="option1" autocomplete="off">
+              <label class="btn btn-outline-warning" for="option1">Batch</label>
+
+              <input type="radio" class="btn-check" v-model="gen.resMode" value="stream" id="option2"
+                autocomplete="off">
+              <label class="btn btn-outline-warning" for="option2">Stream</label>
+            </div>
+            
+            <hr class="text-white">
+            <div class="nav flex-column nav-pills nav-warning">
+        
+              <a class="nav-link d-flex justify-content-between bg-warning text-dark" type="button" href="#" data-bs-toggle="collapse" data-bs-target="#modelParameter"
+                  aria-expanded="false" aria-controls="collapseExample"  aria-current="true">
+                {{ params.selectedModel }}
+                <span class="me-0" ><i class="bi bi-caret-down"></i></span>
+              </a>
+
+              <div class="collapse show" id="modelParameter">
+                <div class="row justify-content-center ms-3">
+                  <div class="col-12 my-2">
+                    <SliderControl v-model="params.modelParams.temperature" label="Temperature" :min="0" :max="1"
+                      :step=".1" @change="params.changeModelParams" :caliber="params.modelParams.temperature" />
+                  </div>
+
+                  <div class="col-lg-12 my-2">
+                    <SliderControl v-model="params.modelParams.top_p" label="Top_p" :min="0" :max="1.0" :step="0.05"
+                      @change="params.changeModelParams" :caliber="params.modelParams.top_p" />
+                  </div>
+
+                  <div class="col-lg-12 my-2">
+                    <SliderControl v-model="params.modelParams.top_k" label="Top_k" :min="0" :max="100" :step="5"
+                      @change="params.changeModelParams" :caliber="params.modelParams.top_k" />
+                  </div>
+                  <div class="col-lg-12 my-2">
+                    <SliderControl v-model="params.modelParams.n_ctx" label="num_ctx" :min="126" :max="8192" :step="126"
+                      @change="params.changeModelParams" :caliber="params.modelParams.n_ctx" />
+                  </div>
+
+        
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr class="text-white">
+
+        </div>
+</template>
+  
+  <style scoped>
+  /* Add any component-specific styles here */
+  </style>  
